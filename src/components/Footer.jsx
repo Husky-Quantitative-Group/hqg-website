1db5c9cb72f7bda9660cc54a88cb7f3f2781e4c5
josef-karpinski <josef.karpinski@uconn.edu>
2026-02-02 22:57:02 -0500

    chore: add github and ig to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,8 +23,10 @@ function Footer() {
         <div className="footer-section">
           <h4>Connect</h4>
           <ul>
-            <li><a href="https://www.linkedin.com/company/h-q-g/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
             <li><a href="https://github.com/Husky-Quantitative-Group" target="_blank" rel="noopener noreferrer">GitHub</a></li>
+            <li><a href="https://www.linkedin.com/company/h-q-g/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+            <li><a href="https://discord.gg/3KcdBwdyAm" target="_blank" rel="noopener noreferrer">Discord</a></li>
+            <li><a href="https://www.instagram.com/huskyquantitativegroup" target="_blank" rel="noopener noreferrer">Instagram</a></li>
             <li><a href="mailto:connect@uconnquant.com">Email</a></li>
           </ul>
         </div>
