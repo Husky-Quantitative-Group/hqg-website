8c599bbc3c5985bc18120c2da83857bc76c41905
josef-karpinski <josef.karpinski@uconn.edu>
2026-05-06 14:06:26 -0400

    feat: mdx code block languages color rendering

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
 import mdx from '@mdx-js/rollup';
 import react from '@vitejs/plugin-react';
+import rehypePrettyCode from 'rehype-pretty-code';
+
+const prettyCodeOptions = {
+  theme: {
+    light: 'github-light',
+    dark: 'github-dark',
+  },
+  keepBackground: false,
+};
 
 export default defineConfig(({ isSsrBuild }) => ({
-  plugins: [mdx(), react()],
+  plugins: [mdx({ rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]] }), react()],
   base: '/',
   server: {
     allowedHosts: ['uncoerced-an-confidently.ngrok-free.dev'],
